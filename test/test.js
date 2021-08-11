1259394bad3d55eb91e6f09be8abc040396ffd09
mendelor <33259548+mendelor@users.noreply.github.com>
2021-08-12 00:06:46 +1200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,9 @@ const calculator = require("../calculator.js");
 
 describe("Calculator Test", function () {
   describe("add() Test", function () {
-
+    it("add(1,2) should return 3", function () {
+      assert.equal(calculator.add(1, 2), 3);
+    });
 
     it("add('1','2') should return 3", function () {
       assert.equal(calculator.add("1", "2"), 3);
